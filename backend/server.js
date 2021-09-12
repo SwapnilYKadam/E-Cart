b375df9511759e48cfbee0d6809c5ced0a053e90
Swapnil Kadam <swapnil.kadam@tieto.com>
2021-09-12 23:20:51 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import { connectDB } from "./db.js";
 import productRouter from "./Routes/productRouter.js";
@@ -16,6 +17,19 @@ app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/checkout", checkoutRouter);
 
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
